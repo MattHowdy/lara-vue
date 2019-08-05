74b25a508777d487f0b7adeba0473592713730f6
Matt <mate.hodi9@gmail.com>
2019-08-05 11:01:59 +0200

    webpack

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -34,7 +34,9 @@
             </div>
         </form>
     </div>
-
+    
+    <script src="/js/vendor.js"></script>
     <script src="/js/app.js"></script>
+    
 </body>
 </html>
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,67 @@
 let webpack = require('webpack')
 let path = require('path')
-
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    entry : './resources/assets/js/app.js',
+    entry : {
+        app : './resources/assets/js/app.js',
+        vendor : ['vue', 'axios']
+    },
 
     output : {
         path : path.resolve(__dirname, 'public/js'),
-        filename : 'app.js', 
+        filename : '[name].js', 
         publicPath : './public'
     },
 
+    module : {
+        rules : [
+            {
+            test: /\.js$/,
+            exclude : /node_modules/,
+            loader : 'babel-loader'
+            }
+        ]
+    },
     resolve : {
         alias : {
-            'vue$' : 'vue/dist/vue.common.js'
+            //'vue$' : 'vue/dist/vue.common.js' // for webpack1
+            'vue$': 'vue/dist/vue.esm.js' 
         }
     },
 
+    optimization: {
+        minimizer: [new UglifyJsPlugin()],
+      },
 
-    plugins : []
+    plugins : [
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor']
+        })
+    ]
 }
 
 if(process.env.NODE_ENV === 'production'){
     module.exports.plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-            sourcemap: true,
-            compress : {
-                warnings : false
+        // new webpack.optimize.UglifyJsPlugin({
+        //     sourcemap: true,
+        //     compress : {
+        //         warnings : false
+        //     }
+        // })
+        new webpack.DefinePlugin({
+            'process.env' : {
+                NODE_ENV : JSON.stringify('production')
             }
         })
     )
 }
+
+module.exports.plugins.push(
+    new webpack.DefinePlugin({
+        'process.env' : {
+            NODE_ENV : 'production'
+        }
+        
+    })
+)
